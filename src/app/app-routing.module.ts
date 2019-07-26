430d8e00eee6666aab07cec49a516edebb6f26d7
mikeyb <MichaelBrown172@Gmail.com>
2019-07-25 20:19:02 -0400

    Frist Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ReactiveFormsModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder,Validators } from '@angular/forms';
+import {User} from './models/user' ;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'formsexample';
+
+  //form related fields
+  userForm: FormGroup ;
+  roles: Array<string> = [
+    'Guest',
+    'Admin',
+    'Owner',
+    'Operator'
+  ];
+
+   /*
+  user: User = {
+    firstName: 'foo',
+    lastName: 'bar',
+    role: 'Guest',
+    notes: undefined
+  };
+  */
+
+  constructor(){
+    // tslint:disable-next-line: label-position
+    /*
+    this.userForm = this.formBuilder.group({
+      firstName: ['', [Validators.pattern('\w{10}')]],
+      lastName : ['',[Validators.required]],
+      role: ['this.user.role', [Validators.required]],
+      notes: ['this.user.notes', [Validators.maxLength(45)]]
+
+    };
+    */
+  }
+
+  ngOnInit() {
+    this.userForm = new FormGroup({
+        firstName: new FormControl('', Validators.compose([
+          //Validators.required,
+          Validators.pattern('^\\w{10}$')
+      ])),
+        lastName: new FormControl('', Validators.compose([
+          //Validators.required,
+          Validators.pattern('^\\w{10}$')
+        ]))
+    });
+  }
+
+
+  logFormValue(){
+    console.log(this.userForm);
+  }
+
 }
+//R3dB1ack!
